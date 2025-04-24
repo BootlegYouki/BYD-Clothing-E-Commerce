b98593e83593f21dac1b7ee8b69be29a2262c541
BootlegYouki <darrenjade24@gmail.com>
2025-04-24 22:05:08 +0800

    asasdasd

--- shop/index.php
+++ shop/index.php
@@ -26,18 +26,6 @@ function get_setting($key, $default = '') {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>Beyond Doubt Clothing</title> 
-    <meta property="og:type" content="website">
-<meta property="og:url" content="https://byd-clothing.com/">
-<meta property="og:title" content="Beyond Doubt Clothing">
-<meta property="og:description" content="Discover stylish designs and unmatched comfort with our latest collection.">
-<meta property="og:image" content="https://byd-clothing.com/shop/img/logo/logo.webp">
-
-<!-- Twitter -->
-<meta property="twitter:card" content="summary_large_image">
-<meta property="twitter:url" content="https://byd-clothing.com/">
-<meta property="twitter:title" content="Beyond Doubt Clothing">
-<meta property="twitter:description" content="Discover stylish designs and unmatched comfort with our latest collection.">
-<meta property="twitter:image" content="https://byd-clothing.com/shop/img/logo/logo.webp">
     <!-- BOOTSTRAP CSS/JS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="icon" href="img/logo/logo.ico" type="image/x-icon">
