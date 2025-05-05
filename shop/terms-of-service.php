a8724a9c01d8f86fb955480c2a5ec49c65375d9e
BootlegYouki <darrenjade24@gmail.com>
2025-05-05 23:07:17 +0800

    minor fix

--- shop/terms-of-service.php
+++ shop/terms-of-service.php
@@ -22,7 +22,20 @@
     <link rel="stylesheet" href="css/indexstyle.css">
 </head>
 <body>
-<?php include_once 'includes/header.php'; ?>
+    <?php include 'includes/header.php'; ?>
+    <!-- SHOPPING CART MODAL  -->
+    <?php include 'includes/shopcart.php'; ?>
+    <!-- REGISTER MODAL  -->
+     <?php include 'includes/register.php'; ?>
+    <!-- LOGIN MODAL  -->
+    <?php include 'includes/login.php'; ?>
+    <!-- LOGOUT MODAL  -->
+    <?php include 'includes/logout.php'; ?>
+    <!-- SUCCESS MODAL  -->
+    <?php include 'includes/loginsuccess.php'; ?>
+    <?php include 'includes/registersuccess.php'; ?>
+    <!-- TERMS MODAL  -->
+    <?php include 'includes/terms.php'; ?>
 <div class="container my-5 py-5">
     <div class="row justify-content-center py-5">
         <div class="col-lg-10">
@@ -76,3 +89,8 @@
 <?php
 include_once 'includes/footer.php';
 ?>
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="js/url-cleaner.js"></script>
+</body>
+</html>
