203693307882c1466f023840b4d1694f794a132d
BootlegYouki <darrenjade24@gmail.com>
2025-05-06 01:06:36 +0800

    added quicksort

--- shop/functions/productfetching/shop_product-handler.php
+++ shop/functions/productfetching/shop_product-handler.php
@@ -10,7 +10,7 @@ function getShopProducts($conn, $params = []) {
     
     $offset = ($page - 1) * $items_per_page;
     
-    // Build base query
+    // Build base query - removed ORDER BY and LIMIT for array-based sorting
     $base_query = "SELECT p.*, pi.image_url as image
                    FROM products p
                    LEFT JOIN product_images pi ON p.id = pi.product_id AND pi.is_primary = 1
@@ -38,48 +38,12 @@ function getShopProducts($conn, $params = []) {
         }
     }
     
-    // Add sorting logic
-    switch ($sort) {
-        case 'price-asc':
-            $base_query .= " ORDER BY CASE 
-                                WHEN p.discount_price > 0 THEN p.discount_price
-                                ELSE p.original_price 
-                              END ASC";
-            break;
-        case 'price-desc':
-            $base_query .= " ORDER BY CASE 
-                                WHEN p.discount_price > 0 THEN p.discount_price
-                                ELSE p.original_price 
-                              END DESC";
-            break;
-        case 'name-asc':
-            $base_query .= " ORDER BY p.name ASC";
-            break;
-        case 'name-desc':
-            $base_query .= " ORDER BY p.name DESC";
-            break;
-        case 'discount':
-            $base_query .= " ORDER BY (p.original_price - p.discount_price) DESC";
-            break;
-        default:
-            $base_query .= " ORDER BY p.created_at DESC";
-            break;
-    }
-    
-    // Count total query - for pagination
-    $count_query = str_replace("p.*, pi.image_url as image", "COUNT(*) as total", $base_query);
-    $count_result = mysqli_query($conn, $count_query);
-    $total_items = mysqli_fetch_assoc($count_result)['total'];
-    $total_pages = ceil($total_items / $items_per_page);
-    
-    // Add pagination to the query
-    $base_query .= " LIMIT $offset, $items_per_page";
-    
-    // Execute the final query
+    // Execute the query to get all matching products (without sorting or pagination)
     $result = mysqli_query($conn, $base_query);
+    $all_products = [];
     $products = [];
     
-    // Fetch products with their additional images
+    // Fetch all products with their additional images
     if ($result && mysqli_num_rows($result) > 0) {
         while ($product = mysqli_fetch_assoc($result)) {
             // Get additional images for this product
@@ -98,7 +62,7 @@ function getShopProducts($conn, $params = []) {
                 'id' => $product['id'],
                 'title' => $product['name'],
                 'price' => $product['original_price'],
-                'discount_price' => $product['discount_price'], // Add the discount_price
+                'discount_price' => $product['discount_price'], 
                 'category' => $product['category'],
                 'discount_percentage' => $product['discount_percentage'],
                 'description' => $product['description'],
@@ -106,7 +70,8 @@ function getShopProducts($conn, $params = []) {
                 'additionalImages' => $additionalImages,
                 'sku' => $product['sku'],
                 'is_featured' => $product['is_featured'],
-                'is_new_release' => $product['is_new_release']
+                'is_new_release' => $product['is_new_release'],
+                'created_at' => $product['created_at']
             ];
             
             // Get available sizes for this product
@@ -121,15 +86,131 @@ function getShopProducts($conn, $params = []) {
             }
             
             $formatted_product['availableSizes'] = $available_sizes;
-            $products[] = $formatted_product;
+            $all_products[] = $formatted_product;
         }
     }
+    
+    // Apply PHP-based sorting
+    sortProductsArray($all_products, $sort);
+    
+    // Count total items for pagination
+    $total_items = count($all_products);
+    $total_pages = ceil($total_items / $items_per_page);
+    
+    // Apply pagination to the array
+    $products = array_slice($all_products, $offset, $items_per_page);
     
     return [
         'products' => $products,
         'total_items' => $total_items,
         'total_pages' => $total_pages
     ];
+}
+
+/**
+ * Custom sorting function for product arrays
+ * 
+ * @param array &$products Reference to the products array to be sorted
+ * @param string $sort Sort option
+ * @return void
+ */
+function sortProductsArray(&$products, $sort) {
+    if (count($products) <= 1) {
+        return;
+    }
+    
+    // Use quicksort algorithm
+    quicksortProducts($products, 0, count($products) - 1, $sort);
+}
+
+/**
+ * Quicksort implementation for product arrays
+ * 
+ * @param array &$products Reference to the products array
+ * @param int $left Start index
+ * @param int $right End index
+ * @param string $sort Sort option
+ * @return void
+ */
+function quicksortProducts(&$products, $left, $right, $sort) {
+    if ($left < $right) {
+        // Get the partition index
+        $partitionIndex = partitionProducts($products, $left, $right, $sort);
+        
+        // Sort the left part
+        quicksortProducts($products, $left, $partitionIndex - 1, $sort);
+        // Sort the right part
+        quicksortProducts($products, $partitionIndex + 1, $right, $sort);
+    }
+}
+
+/**
+ * Partition function for quicksort
+ * 
+ * @param array &$products Reference to the products array
+ * @param int $left Start index
+ * @param int $right End index
+ * @param string $sort Sort option
+ * @return int The partition index
+ */
+function partitionProducts(&$products, $left, $right, $sort) {
+    // Choose the rightmost element as pivot
+    $pivot = $products[$right];
+    $i = $left - 1;
+    
+    for ($j = $left; $j < $right; $j++) {
+        // Compare based on sort option
+        if (compareProducts($products[$j], $pivot, $sort) <= 0) {
+            $i++;
+            // Swap elements
+            $temp = $products[$i];
+            $products[$i] = $products[$j];
+            $products[$j] = $temp;
+        }
+    }
+    
+    // Swap the pivot element
+    $temp = $products[$i + 1];
+    $products[$i + 1] = $products[$right];
+    $products[$right] = $temp;
+    
+    return $i + 1;
+}
+
+/**
+ * Compare two products based on sort criteria
+ * 
+ * @param array $a First product
+ * @param array $b Second product
+ * @param string $sort Sort option
+ * @return int Negative if $a < $b, positive if $a > $b, 0 if equal
+ */
+function compareProducts($a, $b, $sort) {
+    switch ($sort) {
+        case 'price-asc':
+            $price_a = ($a['discount_price'] > 0) ? $a['discount_price'] : $a['price'];
+            $price_b = ($b['discount_price'] > 0) ? $b['discount_price'] : $b['price'];
+            return $price_a - $price_b;
+            
+        case 'price-desc':
+            $price_a = ($a['discount_price'] > 0) ? $a['discount_price'] : $a['price'];
+            $price_b = ($b['discount_price'] > 0) ? $b['discount_price'] : $b['price'];
+            return $price_b - $price_a;
+            
+        case 'name-asc':
+            return strcasecmp($a['title'], $b['title']);
+            
+        case 'name-desc':
+            return strcasecmp($b['title'], $a['title']);
+            
+        case 'discount':
+            $discount_a = $a['price'] - ($a['discount_price'] > 0 ? $a['discount_price'] : $a['price']);
+            $discount_b = $b['price'] - ($b['discount_price'] > 0 ? $b['discount_price'] : $b['price']);
+            return $discount_b - $discount_a;
+            
+        default: // default is newest first
+            return strtotime($b['created_at']) - strtotime($a['created_at']);
+    }
 }
 
 function getAllCategories($conn) {
