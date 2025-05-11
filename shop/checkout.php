6945c86d66988b1c742e6094a01085892840419b
BootlegYouki <darrenjade24@gmail.com>
2025-05-11 23:35:55 +0800

    checkout fix

--- shop/checkout.php
+++ shop/checkout.php
@@ -33,7 +33,7 @@ $shipping_fee = 20;
     <title>Checkout - Beyond Doubt Clothing</title> 
     <!-- BOOTSTRAP CSS/JS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="icon" href="img/logo/BYD-removebg-preview.ico" type="image/x-icon">
+    <link rel="icon" href="img/logo/logo.ico" type="image/x-icon">
     <!-- LEAFLET CSS -->
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
     <link rel="stylesheet" href="https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.css" />
@@ -125,19 +125,19 @@ $shipping_fee = 20;
                                 <div class="row g-3">
                                     <div class="col-md-6">
                                         <label for="firstname" class="form-label">First Name</label>
-                                        <input type="text" class="form-control" id="firstname" name="firstname" value="<?= htmlspecialchars($user['firstname'] ?? '') ?>" required>
+                                        <input type="text" class="form-control" id="firstname" name="firstname" value="<?= htmlspecialchars($user['firstname'] ?? '') ?>" readonly required>
                                     </div>
                                     <div class="col-md-6">
                                         <label for="lastname" class="form-label">Last Name</label>
-                                        <input type="text" class="form-control" id="lastname" name="lastname" value="<?= htmlspecialchars($user['lastname'] ?? '') ?>" required>
+                                        <input type="text" class="form-control" id="lastname" name="lastname" value="<?= htmlspecialchars($user['lastname'] ?? '') ?>" readonly required>
                                     </div>
                                     <div class="col-md-6">
                                         <label for="email" class="form-label">Email</label>
-                                        <input type="email" class="form-control" id="email" name="email" value="<?= htmlspecialchars($user['email'] ?? '') ?>" required>
+                                        <input type="email" class="form-control" id="email" name="email" value="<?= htmlspecialchars($user['email'] ?? '') ?>" readonly required>
                                     </div>
                                     <div class="col-md-6">
                                         <label for="phone" class="form-label">Phone Number</label>
-                                        <input type="tel" class="form-control" id="phone" name="phone" value="<?= htmlspecialchars($user['phone_number'] ?? '') ?>" required>
+                                        <input type="tel" class="form-control" id="phone" name="phone" value="<?= htmlspecialchars($user['phone_number'] ?? '') ?>" readonly required>
                                     </div>
                                 </div>
                             </div>
@@ -172,7 +172,7 @@ $shipping_fee = 20;
                                     </div>
                                     <div class="col-md-6">
                                         <label for="zipcode" class="form-label">Postal/ZIP Code</label>
-                                        <input type="text" class="form-control" id="zipcode" name="zipcode" value="<?= htmlspecialchars($user['zipcode'] ?? '') ?>" required>
+                                        <input type="text" class="form-control" id="zipcode" name="zipcode" value="<?= htmlspecialchars($user['zipcode'] ?? '') ?>" pattern="[0-9]*" inputmode="numeric" onkeypress="return /[0-9]/i.test(event.key)" required>
                                     </div>
                                 </div>
                             </div>
