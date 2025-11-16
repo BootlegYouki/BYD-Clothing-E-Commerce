afdb37ca7262be16771b26ad15f13515121eee22
BootlegYouki <darrenjade24@gmail.com>
2025-11-16 14:28:20 +0800

    update chatbot model to meta-llama/llama-3.3-8b-instruct:free

--- shop/js/assistant.js
+++ shop/js/assistant.js
@@ -2,7 +2,7 @@ let currentController = null;
 let currentMessageId = null;
 let latestBotMessageId = null;
 let conversationHistory;
-const chatbot = "deepseek/deepseek-chat-v3-0324";
+const chatbot = "meta-llama/llama-3.3-8b-instruct:free";
 //OPEN CHATBOT
 document.addEventListener('DOMContentLoaded', function() {
     initializeBot();
